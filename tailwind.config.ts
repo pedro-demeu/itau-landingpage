c405f1b35afab8798c20aabd0dec1644b8eaede2
pdemeu <pdemeu.pessoal@gmail.com>
2024-02-05 13:38:32 -0300

    Add new assets and update theme configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    backgroundImage: {
+      img_bg_hero: "url('../assets/bg-hero.jpg')",
+    },
+    extend: {
+      "primary-blue": "#106EB0",
+      "primary-orange": "#EC7000",
+      "primary-gray": "#33303E",
+      "second-gray": "#4E4B59",
+      "gray-phone": "#F4F4F4",
+      "txt-gray": "#7A7786",
+      "opacity-gray": "rgba(100, 80, 57, 0.1)",
+    },
   },
   plugins: [],
 };
