c92164e6ba1abbf1b1596c0da93b9cc9eb6a8645
pdemeu <pdemeu.pessoal@gmail.com>
2024-02-05 21:06:50 -0300

    Update Tailwind config with new colors and asset path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,16 +8,18 @@ const config: Config = {
   ],
   theme: {
     backgroundImage: {
-      img_bg_hero: "url('../assets/bg-hero.jpg')",
+      img_bg_hero: "url('./assets/bg-hero.jpg')",
     },
     extend: {
-      "primary-blue": "#106EB0",
-      "primary-orange": "#EC7000",
-      "primary-gray": "#33303E",
-      "second-gray": "#4E4B59",
-      "gray-phone": "#F4F4F4",
-      "txt-gray": "#7A7786",
-      "opacity-gray": "rgba(100, 80, 57, 0.1)",
+      colors: {
+        "primary-blue": "#106EB0",
+        "primary-orange": "#EC7000",
+        "primary-gray": "#33303E",
+        "second-gray": "#4E4B59",
+        "gray-phone": "#F4F4F4",
+        "txt-gray": "#7A7786",
+        "opacity-gray": "rgba(100, 80, 57, 0.1)",
+      },
     },
   },
   plugins: [],
